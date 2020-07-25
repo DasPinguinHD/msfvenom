326697940ade8f8bbeba2b2763b9ab2bb1b3197d
DasPinguinHD <66183561+DasPinguinHD@users.noreply.github.com>
2020-07-25 10:48:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-cp msfg.sh /bin/sh/msfg.sh
-chmod +x /bin/sh/msfg.sh
-
+mkdir /usr/bin/msfg
+echo Directory created
+cp msfg.sh /usr/bin/msfg/msfg.sh
+echo startup file created
+cp msfvenomgenerator.py /usr/bin/msfg/msfg.py
+echo script file created
+echo "alias msfg='/usr/bin/msfg.sh'" >> ~/.bashrc
+chmod +x /usr/bin/msfg.sh
+echo Done! Restart your terminal now!
+source ~/.bashrc
